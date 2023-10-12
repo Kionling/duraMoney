a24b08040936458161ce323eeee7b3e218c85f92
kionling <djvdjv209@gmail.com>
2023-10-11 22:57:20 -0700

    Modified login page

--- client/src/pages/Login/login.js
+++ client/src/pages/Login/login.js
@@ -1,24 +1,20 @@
 import react from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-
-
-function Login(){
-    return(
-        <div>
-            <div id="formContainer">
-                <h1>Form div</h1>
-            </div>
-            <div id="imageContainer">
-                <h1>Image div</h1>
-            </div>
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../Login/login.css"
+function Login() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col s6">
+          <div id="formContainer">
+            <h1>Form div</h1>
+          </div>
         </div>
-    )
+        <div className="col s6" id="imageContainer">
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Login;
